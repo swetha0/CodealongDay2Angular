def2d1b7609fce693650c1c1f6a8634f61cb6ed7
swetha0 <gaali.swetha@gmail.com>
2020-06-09 20:15:25 -0400

    FSJ0420A104

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-
-const routes: Routes = [];
+import {HomeComponent} from './home/home.component'
+const routes: Routes = [
+  {path: '',component:HomeComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
